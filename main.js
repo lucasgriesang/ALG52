f47380284b388d4bf22ed151b6e885c86582ff59
lucasgriesang <127210973+lucasgriesang@users.noreply.github.com>
2023-08-08 09:08:40 -0300

    d

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function ad() {
-    var nome = document.getElementById("admin").value
+    let nome = document.getElementById("admin").value
     if (nome === "admin") {
         window.alert("o nome esta coreto ");
     }
@@ -7,7 +7,7 @@ function ad() {
         alert("nome incoreto ");
     }
     function senha()
-    var senha = document.getElementById("123").value
+    let senha = document.getElementById("123").value
     if (senha === "123") {
         window.alert("o senha esta coreto ");
     }
