2eb1e4d4e626d14b9f2ce60c171504c680108e69
lucasgriesang <127210973+lucasgriesang@users.noreply.github.com>
2023-08-08 09:04:05 -0300

    f

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function ad() {
     else {
         alert("nome incoreto ");
     }
-    function senha()
     var senha = document.getElementById("123").value
     if (senha === "123") {
         window.alert("o senha esta coreto ");
