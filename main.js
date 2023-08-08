eb86da1e77c55ba6c47d9e55cd0c80f695dc377b
lucasgriesang <127210973+lucasgriesang@users.noreply.github.com>
2023-08-08 08:15:19 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
+function handleAddName() {
+    //pega o valor digitado pelo usuário pq tem o username como id
+    let userName = document.querySelector("#username").value
+    //console.log(userName.value)
 
-function ad() {
-    var nome = value
-    var nome = document.getElementById("adimin").value
-    if (nome === "adimin") {
-        window.alert("o nome esta coreto ");
-    }
-    else {
-        alert("nome incoreto ");
-    }
+    //envia o valor captura para o localstorage
+    localStorage.setItem("username", userName);
+    //localStorage.setItem("password", "123456");
 }
+
+
+
+function handleAddName() {
+    //pega o valor digitado pelo usuário pq tem o username como id
+    let userName = document.querySelector("#senha").value
+    //console.log(userName.value)
+
+    //envia o valor captura para o localstorage
+    localStorage.setItem("senha", userName);
+    //localStorage.setItem("password", "123456");
+}
+
